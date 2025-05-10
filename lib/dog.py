71f91d3d9409a20ae50245799459542b855aeea7
judgeOstrich <jeraldnyaga15@gmail.com>
2025-05-10 15:35:32 +0300

    Objects initialization assignments

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt",favourite_toy="Any"):
+        self.name = name
+        self.favourite_toy = favourite_toy
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, ownner_name):
+        self.owner = ownner_name
+
+    def showing_self(self):
+        return self
+
+fido = Dog("Fido")
+snoopy = Dog("Snoopy")
+
+print(fido.name)
+print(snoopy.name)
+
+fido.get_adopted("Mark")
+snoopy.get_adopted("Susan")
+print(fido.owner)
+print(snoopy.owner)
